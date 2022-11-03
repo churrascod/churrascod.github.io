8c4b746113afd786770e9b161c899d7e1b2eeb42
churrascod <117380347+churrascod@users.noreply.github.com>
2022-11-03 13:29:54 -0300

    Add files via upload

---
+++ click.js
@@ -0,0 +1,10 @@
+function umClique(){
+  document.getElementById("imagem").src="C:\Users\Usuario\Desktop\bf42\B9729432295Z.1_20211227103142_000+GPTJJ5DO8.2-0.png";
+  
+}
+
+
+function doisCliques(){
+  document.getElementById("imagem").src="C:\Users\Usuario\Desktop\bf42\Battlefield-2042-Breakaway-map-screenshot.jpg";
+  
+}
